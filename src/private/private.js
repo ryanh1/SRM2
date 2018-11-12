11a113a333a109b69043199cc0ade646769289e5
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2018-11-11 18:58:19 -0800

    Tracking private in master branch

---
+++ src/private/private.js
@@ -0,0 +1,9 @@
+// Firebase config
+export const firebaseConfiguration = {
+  apiKey: "AIzaSyAhk15GjFhUuJ0HBL2zCxfuPZyKaOxcJ7w",
+  authDomain: "srm2-rsh.firebaseapp.com",
+  databaseURL: "https://srm2-rsh.firebaseio.com",
+  projectId: "srm2-rsh",
+  storageBucket: "srm2-rsh.appspot.com",
+  messagingSenderId: "524959536418"
+};
