96d9675f257a97c5334475e4fb2b122e223d8b9c
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2019-01-06 12:09:55 -0800

    Add locations

--- src/components/TopPanel.js
+++ src/components/TopPanel.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LocationsSelector from './LocationsSelector';
+import LocationsSelectorModal from './LocationsSelectorModal';
 import AddFriendPage from './AddFriendPage';
 
 const TopPanel = (props) => (
   <div className="border border-secondary mb-4 p-3 d-flex">
-    <div className="ml-3 mt-2 mb-3 mr-2">
-      { props.locations.length > 0 ? (<h3>Locations</h3>) : (<div></div>) }
-      <LocationsSelector />
+    <div className="ml-3 mt-5 mb-3 mr-2">
+      { /* props.locations.length > 0 ? (<h3></h3>) : (<div> </div>) */ }
+      <LocationsSelectorModal />
     </div>
     <div className="text-center">
       <h3 className="ml-3 mt-2 mr-2 m-2">Add a new friend</h3>
