e70ca94199ba77c3fbb3fa719429e122ab50e249
Ryan Hollander <ryanhollander@ryan-hollanders-macbook-pro.local>
2019-04-05 23:49:03 -0700

    update master

---
+++ src/private/private.js
@@ -0,0 +1,26 @@
+// Firebase config
+export const firebaseConfiguration = {
+  apiKey: "AIzaSyAhk15GjFhUuJ0HBL2zCxfuPZyKaOxcJ7w",
+  authDomain: "srm2-rsh.firebaseapp.com",
+  databaseURL: "https://srm2-rsh.firebaseio.com",
+  projectId: "srm2-rsh",
+  storageBucket: "srm2-rsh.appspot.com",
+  messagingSenderId: "524959536418"
+};
+
+export const AmplitudeApiKey = "4dbe07f76a4ab526b60a30b7c7f0bfac";
+
+/*
+To do:
+
+Technologies used:
+-Everything in package.json
+-Bootstrap
+-Amplitude
+-Google Analytics
+-Quantcast measure
+-Google Tag Manager
+-Appcues
+-Favicon icon
+
+*/
